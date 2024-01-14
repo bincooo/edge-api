47db99aab98795af0e3c8d759730f9eaf993348f
bincooo <bincooo@iMac.local>
2024-01-14 22:14:47 +0800

    feat: 更新版本

--- vars.go
+++ vars.go
@@ -1,9 +1,12 @@
 package edge
 
-import _ "embed"
+import (
+	_ "embed"
+	"github.com/joho/godotenv"
+	"os"
+)
 
 const (
-	Version = "1.1418.9-suno"
 	//DefaultCreate  = "https://www.bing.com/turing/conversation/create"
 	DefaultCreate  = "https://copilot.microsoft.com/turing/conversation/create"
 	DefaultChatHub = "wss://sydney.bing.com/sydney/ChatHub"
@@ -18,6 +21,8 @@ const (
 
 //go:embed chat.json
 var chatHub []byte
+
+var Version = "1.1467.6"
 
 var (
 	sliceIds = []string{
@@ -46,3 +51,16 @@ var (
 	schema = []byte{123, 34, 112, 114, 111, 116, 111, 99, 111, 108, 34, 58, 34, 106, 115, 111, 110, 34, 44, 34, 118, 101, 114, 115, 105, 111, 110, 34, 58, 49, 125, 30}
 	ping   = []byte{123, 34, 116, 121, 112, 101, 34, 58, 54, 125, 30}
 )
+
+func init() {
+	_ = godotenv.Load()
+	Version = LoadEnvVar("BING_VER", Version)
+}
+
+func LoadEnvVar(key, defaultValue string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		value = defaultValue
+	}
+	return value
+}
