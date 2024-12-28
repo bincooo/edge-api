a3b98723b370758f67488d064fd4bdd0ece1e7b8
bincooo <bincooo@bincooos-iMac.local>
2024-12-29 04:18:04 +0800

    update

--- chat.go
+++ chat.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"net/http"
 
 	"github.com/RomiChan/websocket"
@@ -14,9 +16,11 @@ var (
 	userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.1.1 Safari/605.1.15"
 
 	// 懒得定义了
-	H = []byte("{\"event\":\"setOptions\",\"ads\":{\"supportedTypes\":[\"text\",\"propertyPromotion\",\"tourActivity\",\"product\",\"multimedia\"]}}")
-	D = []byte("{\"event\":\"done\"")
-	E = []byte("{\"event\":\"error\"")
+	H  = []byte("{\"event\":\"setOptions\",\"ads\":{\"supportedTypes\":[\"text\",\"propertyPromotion\",\"tourActivity\",\"product\",\"multimedia\"]}}")
+	D  = []byte("{\"event\":\"done\"")
+	E  = []byte("{\"event\":\"error\"")
+	C  = []byte("{\"event\":\"challenge\"")
+	C3 = []byte("{\"event\":\"challenge\",\"id\":\"2\"}")
 )
 
 type Msg struct {
@@ -27,6 +31,13 @@ type Msg struct {
 		Type string `json:"type"`
 		Text string `json:"text"`
 	} `json:"content"`
+	Context *struct {
+		Edge *struct {
+			PageTitle   string `json:"pageTitle"`
+			PageContent string `json:"pageContent"`
+			PageUrl     string `json:"pageUrl"`
+		} `json:"edge"`
+	} `json:"context"`
 }
 
 func DeleteConversation(session *emit.Session, ctx context.Context, conversationId, accessToken string) (err error) {
@@ -72,11 +83,14 @@ func CreateConversation(session *emit.Session, ctx context.Context, accessToken 
 	return
 }
 
-func Chat(session *emit.Session, ctx context.Context, accessToken, conversationId, query string) (message chan []byte, err error) {
+func Chat(session *emit.Session, ctx context.Context, accessToken, conversationId, challenge, file, query string) (message chan []byte, err error) {
 	conn, _, err := emit.SocketBuilder(session).
 		Context(ctx).
 		URL("wss://copilot.microsoft.com/c/api/chat").
 		Query("api-version", "2").
+		Query("features", "-,ncedge,edgepagecontext").
+		Query("setflight", "-,ncedge,edgepagecontext").
+		Query("ncedge", "1").
 		Query(elseOf(accessToken != "", "accessToken"), accessToken).
 		Header("origin", "https://copilot.microsoft.com").
 		Header("user-agent", userAgent).
@@ -98,25 +112,57 @@ func Chat(session *emit.Session, ctx context.Context, accessToken, conversationI
 			Type string `json:"type"`
 			Text string `json:"text"`
 		}{{Type: "text", Text: query}},
+		Context: &struct {
+			Edge *struct {
+				PageTitle   string `json:"pageTitle"`
+				PageContent string `json:"pageContent"`
+				PageUrl     string `json:"pageUrl"`
+			} `json:"edge"`
+		}{Edge: &struct {
+			PageTitle   string `json:"pageTitle"`
+			PageContent string `json:"pageContent"`
+			PageUrl     string `json:"pageUrl"`
+		}{
+			PageUrl:     "/usr/home/histories.txt",
+			PageTitle:   "chat histories",
+			PageContent: "```\n\"\"\"\n" + file + "\"\"\"\n```\n",
+		}},
 	}
 	hexBytes, err := json.Marshal(request)
 	if err != nil {
 		return
 	}
-	err = conn.WriteMessage(websocket.TextMessage, hexBytes)
+
+	cb := func() {
+		conn.WriteMessage(websocket.TextMessage, hexBytes)
+	}
+	cb()
+
+	_, p, err := conn.ReadMessage()
 	if err != nil {
 		return
 	}
 
+	if challenge == "" && bytes.HasPrefix(p, C) {
+		err = errors.New("challenge")
+		return
+	}
+
+	if bytes.HasPrefix(p, E) {
+		err = fmt.Errorf("%s", p)
+		return
+	}
+
 	message = make(chan []byte)
-	go resolve(conn, message)
+	go resolve(conn, challenge, cb, message)
 	return
 }
 
-func resolve(conn *websocket.Conn, message chan []byte) {
+func resolve(conn *websocket.Conn, challenge string, cb func(), message chan []byte) {
 	defer close(message)
 	defer conn.Close()
 
+	ig := false
 	for {
 		_, chunk, err := conn.ReadMessage()
 		if err != nil {
@@ -132,6 +178,26 @@ func resolve(conn *websocket.Conn, message chan []byte) {
 			message <- messageBuffer(1, chunk)
 		}
 
+		if !ig && bytes.Equal(chunk, C3) {
+			if challenge != "" {
+				hex, e := json.Marshal(map[string]string{
+					"method": "cloudflare",
+					"event":  "challengeResponse",
+					"token":  challenge,
+				})
+				if e != nil {
+					return
+				}
+				err = conn.WriteMessage(websocket.TextMessage, hex)
+				if err != nil {
+					return
+				}
+				ig = true
+				cb()
+				continue
+			}
+			message <- messageBuffer(1, "challenge")
+		}
 		message <- messageBuffer(0, chunk)
 	}
 }
